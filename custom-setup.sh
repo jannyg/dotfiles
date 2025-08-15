a36373c72f2ab38283d1a8bc3d9223e9eed5a7b4
jan-arve.nygard@bidbax.no <jan-arve.nygard@bidbax.no@jan-arvenygardbidbaxnos-MacBook-Pro.local>
2025-08-15 10:26:39 +0200

    Added custom setup script

---
+++ custom-setup.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Create nvim config directory
+
+directory=~/.config
+if [ -d "$directory" ]
+then
+    echo "Copying config..."
+    cp -a nvim ~/.config/
+else
+    echo "Folder does not exist. Creating..."
+    mkdir ~/.config
+    echo "Copying config..."
+    cp -a nvim ~/.config/
+fi
+
+# Create tmux/tmuxinator setup
+cp .tmux.conf ~/
+cp -a tmuxinator ~/.config/
+
+# Create cursor setup
+
+# Create gitconfig
+cp .gitconfig ~/
+cp .gitignore ~/
+
+# Create curl config
+cp .curlrc ~/
+# Create wget config
+cp .wgetrc ~/
+# Create vim config
+cp .vimrc ~/
+cp -a .vim ~/
+# Create screen config
+cp .screenrc ~/
