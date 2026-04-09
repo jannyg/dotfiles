8552791969c4b59cde1aee160eded725e1c9cb19
jan-arve.nygard@bidbax.no <jan@ridabu.no>
2026-04-09 08:07:55 +0200

    Show request timeouts in pingcheck.sh

--- tools/pingcheck.sh
+++ tools/pingcheck.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
- ping 8.8.8.8 | grep --line-buffered 'time=' | awk -F'time=' '{
-       time_str = $2
-       match(time_str, /[0-9.]+/)
-       time_val = substr(time_str, RSTART, RLENGTH) + 0
+ ping 8.8.8.8 | grep --line-buffered -E 'time=|Request timeout|No route' | awk -F'time=' '{
+       if ($0 ~ /Request timeout/ || $0 ~ /No route/) {
+           printf "\033[31mRequest timeout\033[0m\n"
+       } else {
+           time_str = $2
+           match(time_str, /[0-9.]+/)
+           time_val = substr(time_str, RSTART, RLENGTH) + 0
 
-       if (time_val >= 200)
-           color = "\033[31m"  # Red
-       else if (time_val >= 100)
-           color = "\033[33m"  # Yellow
-       else
-           color = "\033[32m"  # Green
+           if (time_val >= 200)
+               color = "\033[31m"  # Red
+           else if (time_val >= 100)
+               color = "\033[33m"  # Yellow
+           else
+               color = "\033[32m"  # Green
 
-       printf "%stime=%s\033[0m\n", color, time_str
+           printf "%stime=%s\033[0m\n", color, time_str
+       }
        fflush()
    }'
 
